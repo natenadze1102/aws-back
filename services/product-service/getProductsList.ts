191d633b41a5c9fdf5573bb9a56ad6b7b1076148
natenadze1102 <natenadze1102@gmail.com>
2025-02-21 13:47:33 +0400

    fix products objects

--- services/product-service/getProductsList.ts
+++ services/product-service/getProductsList.ts
@@ -2,8 +2,9 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async () => {
   const products = [
-    { id: '1', name: 'ProductA', price: 100 },
-    { id: '2', name: 'ProductB', price: 200 },
+    { id: '1', title: 'ProductA', description:'This is product A description', price: 100 },
+    { id: '2', title: 'ProductB',description:'This is product B description', price: 200 },
+    { id: '3', title: 'ProductC',description:'This is product C description', price: 300 },
   ];
 
   return {
